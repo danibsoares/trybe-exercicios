24bfd6cc1ec581d65c45d278f3dae94362d9a59f
danibsoares <daniellabsoares@live.com>
2022-12-04 12:40:36 -0300

    exercício 7

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-introducao-a-javascript---primeiros-passos/scripts7.js
@@ -0,0 +1,8 @@
+const a = 23;
+const b = 17;
+
+console.log('Resultado de ' + a + ' + ' + b + ' = ' + (a + b));
+console.log('Resultado de ' + a + ' - ' + b + ' = ' + (a - b));
+console.log('Resultado de ' + a + ' *' + b + ' = ' + (a * b));
+console.log('Resultado de ' + a + ' / ' + b + ' = ' + (a / b));
+console.log('Resultado de ' + a + ' % ' + b + ' = ' + (a % b));
