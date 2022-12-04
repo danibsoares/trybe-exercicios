2fb65736c439f6daffa1b01355525ea8ca72ceb6
danibsoares <daniellabsoares@live.com>
2022-12-04 13:41:08 -0300

    adicionado exercício 14

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-introducao-a-javascript---primeiros-passos/scripts14.js
@@ -0,0 +1,14 @@
+const a = 19;
+const b = 12;
+const c = 86;
+let resultado;
+
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+    resultado = true;
+}
+
+else {
+    resultado = false;
+}
+
+console.log(resultado);
