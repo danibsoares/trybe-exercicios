bf808cd4940f8147b82eded03160639de654c3ce
danibsoares <daniellabsoares@live.com>
2022-12-20 15:02:42 -0300

    primeiro exercício

---
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/exercicio1.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = "A Mulher Rei";
+
+document.getElementById("second-paragraph").innerText = "Este é o segundo parágrafo";
+
+document.getElementById("subtitle").innerText = "este é o subtítulo da página";
